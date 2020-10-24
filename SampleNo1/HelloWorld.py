bc223b808bbb99e4336fbabecff860864d86769f
Conan Hdk <conan.hdk@gmail.com>
2020-10-24 19:13:15 +0900

    python で　LibreOffice へアクセスするサンプル

---
+++ SampleNo1/HelloWorld.py
@@ -0,0 +1,25 @@
+'''
+Created on 2020/10/24
+
+@author: conan
+'''
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+def main():
+    logger.debug('debug message!')
+    logger.info('info message!')
+    logger.warning('warning message.')
+    logger.error('error message!')
+    logger.critical('critical message!')
+
+if __name__ == '__main__':
+    def loggerInit(logger):
+        import logging
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.DEBUG)
+        logger.addHandler(sh)
+        logger.setLevel(logging.DEBUG)
+    loggerInit(logger)
+    main()
